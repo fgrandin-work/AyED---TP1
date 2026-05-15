46e6e94daf18ef1a7eb2f0df9c92b69a04f3b31b
con_grandinf <francisco.grandin@sancristobal.com.ar>
2026-05-15 13:50:43 -0300

    solucionado reglas numero secreto

--- TP1.py
+++ TP1.py
@@ -200,6 +200,7 @@ def JuegoNumeroSecreto():
         finDeJuego= 0
     elif(opcionmenujuego == 'b'):
         ReglasNumeroSecreto()
+        finDeJuego = 0
     elif(opcionmenujuego == 'c'):
         finDeJuego = 1  
     else:
